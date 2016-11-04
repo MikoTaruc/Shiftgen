96a5d483e78a8047eac96641f0ee132b82a3cf91
Miko Taruc <miko.taruc@gmail.com>
2016-11-03 21:30:23 -0400

    creates shifts in 4/5 blocks

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,5 +1,4 @@
 var Calendar = {
-
   init: function(){
     // moustache style templating
     _.templateSettings = {
@@ -44,20 +43,53 @@ var Calendar = {
     return Calendar.calendar.fullCalendar("clientEvents");
   },
 
-  calculateDaysInMonth: function(year, month) {
-    var monthStart = new Date(year, month, 1);
-    var monthEnd = new Date(year, month + 1, 1);
-    var monthLength = (monthEnd - monthStart) / (1000 * 60 * 60 * 24);
-    return monthLength;
+  generateBlock: function(y, m, d, blockSize) {
+    var events = [];
+    var shift;
+    for (var i = 0; i < blockSize; i++) {
+      shift = new Date(y, m, d);
+      var int_year = shift.getFullYear();
+      var int_month = shift.getMonth();
+      var int_date = shift.getDate();
+      events.push({
+        title: 'shift',
+        // Plus 1 to month because FullCalendar has January as 1 instead of 0.
+        start: int_year+ '-' + Calendar.pad(int_month + 1) + '-' + Calendar.pad(int_date),
+        extras: {
+          year: int_year,
+          month: int_month,
+          date: int_date
+        }
+      });
+      d++;
+    }
+    return events;
+  },
+
+  pad: function(num){
+    if (num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
   },
 
   addShifts: function(y, m, d) {
     var int_year = parseInt(y);
-    var int_month = parseInt(m);
+    // Subtract 1 because javascript Date has January as 0
+    var int_month = parseInt(m - 1);
     var int_day = parseInt(d);
-    Math.floor(Calendar.calculateDaysInMonth(int_year, int_month));
+    var events = [];
+    var first_run = true;
+    var break_size = 5;
+    while(first_run || last_day.extras.year === int_year) {
+      events = events.concat(Calendar.generateBlock(int_year, int_month, int_day, 4));
+      var last_day = events[events.length-1];
+      int_month = last_day.extras.month;
+      int_day = last_day.extras.date + 1 + break_size;
+      first_run = false;
+    }
 
-    date = _.template("{{ year }}-{{ month }}-{{ day }}");
-    $('#calendar').fullCalendar('renderEvent', {title: 'Shift', start: date({year: y, month: m, day: d})});
+    $('#calendar').fullCalendar("addEventSource", events);
   }
 };
