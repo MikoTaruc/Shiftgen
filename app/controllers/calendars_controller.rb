7ad03ae539f253da8905c982abcbdc4de2b0ffeb
Miko Taruc <miko.taruc@gmail.com>
2017-01-18 08:12:40 -0500

    Sync button prototype

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,4 +3,9 @@ require 'pp'
 class CalendarsController < ApplicationController
   def index
   end
+
+  def google_sync
+    pp "Syncing with google TEST"
+    render :json => {}, :status => 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'calendars#index'
+  post 'google_sync', to: 'calendars#google_sync', as: 'sync'
 end
